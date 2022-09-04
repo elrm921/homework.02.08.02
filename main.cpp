7fcb5f10751ec4db6077389b09fa4ad644c2abd3
elrm921 <pocketrocket03@gmail.com>
2022-09-04 23:20:09 +0300

    first commit

---
+++ main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <vector>
+#include <list>
+#include <set>
+#include <algorithm>
+
+template<typename T>
+void print_container(T container) {
+    auto first = container.begin();
+    auto last = container.end();
+    while (first != last) {
+        std::cout << *(first++) << " ";
+    }
+    std::cout << std::endl;
+}
+
+int main() {
+    std::set<std::string> test_set = { "one", "two", "three", "four" };
+    print_container(test_set);
+    std::list<std::string> test_list = { "one", "two", "three", "four" };
+    print_container(test_list);
+    std::vector<std::string> test_vector = { "one", "two", "three", "four" };
+    print_container(test_vector);
+
+    return 0;
+}
